e7f71e055f8f8a2db0c56e02641a38cb5e713b72
Wind_ContestantPC <shiow620412@gmail.com>
2018-10-01 16:03:24 +0800

    upload

--- 104/skills-104-4/Program.cs
+++ 104/skills-104-4/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace skills_104_4
 {
+ 
     class Program
     {
+
+        static model[] m;
         static void Main(string[] args)
         {
             do
@@ -30,10 +33,51 @@ namespace skills_104_4
         }
         static void Func1()
         {
-
+            m = new model[int.Parse(input("輸入模型資料，總筆數為："))];
+            Console.Write("    序列( x軸)：");
+            for (int i = 0; i < m.Length; i++) Console.Write( i.ToString().PadLeft(2));
+            Console.Write("\n");
+            int[] up, mid, down;
+            up = Array.ConvertAll(input("數值串列(上限)：").Split(' '), int.Parse);
+            mid = Array.ConvertAll(input("數值串列(中心)：").Split(' '), int.Parse);
+            down = Array.ConvertAll(input("數值串列(下限)：").Split(' '), int.Parse);
+            for (int i = 0; i < m.Length; i++)
+            {
+                m[i] = new model();
+                m[i].up = up[i];
+                m[i].mid = mid[i];
+                m[i].down = down[i];
+            }
         }
         static void Func2()
         {
+            if( m == null)
+            {
+                int[] up, mid, down;
+                string[] temp = File.ReadAllLines("Q4_InputModel.txt");
+                up = Array.ConvertAll(temp[0].Split(' '), int.Parse);
+                mid = Array.ConvertAll(temp[1].Split(' '), int.Parse);
+                down = Array.ConvertAll(temp[2].Split(' '), int.Parse);
+                m = new model[up.Length];
+                for (int i = 0; i < m.Length; i++)
+                {
+                    m[i] = new model();
+                    m[i].up = up[i];
+                    m[i].mid = mid[i];
+                    m[i].down = down[i];
+                }
+            }
+            string file = input("請輸入「資料串列」檔名：");
+            int[] data = Array.ConvertAll(File.ReadAllText(file).Split(' '),int.Parse);
+            Console.WriteLine($"已開啟「資料串列」檔名：{file}");
+            float output = 0;
+            for(int i =0;i<m.Length;i++)
+            {
+                output += m[i].getSimilar(data[i]);
+            }
+            output /= m.Length;
+            Console.WriteLine($"平均相似度為{output}");
+
 
         }
         static void Func3()
@@ -45,5 +89,45 @@ namespace skills_104_4
             Console.Write(str);
             return Console.ReadLine();
         }
+        public  class model
+        {
+            public int up;
+            public int mid;
+            public int down;
+            public float getSimilar(int val)
+            {
+                if (val == mid)
+                {
+                    return 1;
+                }
+                else if (val == up | val == down)
+                    return 0;
+                else if(val > mid)
+                {
+                    float add = 1f / (mid -up);
+                    float output = 1;
+                    int middle = mid;                    
+                    while(middle != val)
+                    {
+                        middle++;
+                        output -= add;
+                    }
+                    return output;
+                }
+                else
+                {
+                    float add = 1f / (mid - down);
+                    float output = 0;
+                    int dn = down;
+                    while (dn != val)
+                    {
+                        dn++;
+                        output += add;
+                    }
+                    return output;
+                }
+            }
+        }
     }
+    
 }
